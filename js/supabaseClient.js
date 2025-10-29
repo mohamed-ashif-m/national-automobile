115f1c7c64da4f461b9b79afc25b8df2cc4b177c
MOHAMED ASHIF <159855051+mohamed-ashif-m@users.noreply.github.com>
2025-10-29 11:51:09 +0530

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,6 @@
 // js/supabaseClient.js
 
-const SUPABASE_URL = "https://hjcsatyzjwxpqgfimwvh.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhqY3NhdHl6and4cHFnZmltd3ZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5MTg0MDUsImV4cCI6MjA2ODQ5NDQwNX0.L1n-vvn2h2CCtSjg6YPoZrytFkObixxOMCxvIqzTOoo";
+const SUPABASE_URL = "";
+const SUPABASE_ANON_KEY = "";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
